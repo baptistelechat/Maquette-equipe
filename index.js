caa0e7ca9c72593aa63f02f6f2208627fd0f0430
Baptiste LECHAT <baptistelechat@outlook.fr>
2022-06-23 11:00:30 +0200

    add animation on delivery__arrow button

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 const arrow = document.querySelector(".delivery__arrow");
 
+window.onscroll = () => {
+  displayBtn();
+};
+
+const displayBtn = () => {
+  console.log(arrow.classList.value);
+
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    arrow.classList.value = "delivery__arrow enterAnimation";
+  } else if (arrow.classList.value === "delivery__arrow enterAnimation") {
+    arrow.classList.value = "delivery__arrow exitAnimation";
+  } else if (
+    arrow.classList.value !== "delivery__arrow enterAnimation" &&
+    arrow.classList.value !== "delivery__arrow exitAnimation"
+  ) {
+    arrow.classList.value = "delivery__arrow";
+  }
+};
+
 arrow.addEventListener("click", (e) => {
   window.scrollTo({ top: 0, bevahior: "smooth" });
 });
